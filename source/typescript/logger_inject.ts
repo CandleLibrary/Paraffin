87d37c1a6ba8e7a60be4a854b517fd81a2b9be83
Anthony Weathersby <acweathersby.codes@gmail.com>
2021-10-07 11:34:27 -0600

    Feat: Add log rewrite handler for NodeJS

--- source/typescript/logger_inject.ts
+++ source/typescript/logger_inject.ts
@@ -2,6 +2,8 @@ import { LogLevel, LogWriter } from "@candlelib/log";
 
 import { col_x11, xtBlink, xtBold, xtColor, xtDim, xtF, xtInvert, xtRBold, xtReset } from "./color/color.js";
 
+import { inspect } from 'util';
+
 const log_color = xtF(xtColor(col_x11.SkyBlue2));
 const error_color = xtF(xtBlink, xtBold, xtColor(col_x11.Red3));
 const warn_color = xtF(xtColor(col_x11.OrangeRed));
@@ -10,30 +12,65 @@ const time_color = xtF(xtColor(col_x11.LemonChiffon1));
 const rst = xtF(xtReset);
 const dim = xtF(xtDim);
 
+let cursor;
+
+let lines = 0;
+let delta = 0;
+
 LogWriter.prototype.writeLog = function (
     logger_name: string,
     log_level: LogLevel,
+    REWRITE: boolean,
     ...args: any[]
 ) {
 
+    if (REWRITE) {
+        //Clear the last lines added
+    }
+
+    const data = args.map(i => typeof i == "string" ? i : inspect(i, {
+        depth: 8, colors: true
+    })).join(" ");
+
+    let header = "";
 
     switch (log_level) {
         case LogLevel.CRITICAL:
-            console.error(construct_log_header(xtF(xtBold) + "CRITICAL" + xtF(xtRBold), logger_name, "❗", error_color), ...args);
+            header = construct_log_header(xtF(xtBold) + "CRITICAL" + xtF(xtRBold), logger_name, "❗", error_color);
             break;
         case LogLevel.ERROR:
-            console.error(construct_log_header("ERROR", logger_name, "❌", error_color), ...args);
+            header = construct_log_header("ERROR", logger_name, "❌", error_color);
             break;
         case LogLevel.WARN:
-            console.warn(construct_log_header("WARN", logger_name, "⚠️", warn_color), ...args);
+            header = construct_log_header("WARN", logger_name, "⚠️", warn_color);
             break;
         case LogLevel.INFO:
-            console.info(construct_log_header("INFO", logger_name, "ℹ️", log_color), ...args);
+            header = construct_log_header("INFO", logger_name, "ℹ️", log_color);
             break;
         case LogLevel.DEBUG:
-            console.debug(construct_log_header("DEBUG", logger_name, "☣️", debug_color), ...args);
+            header = construct_log_header("DEBUG", logger_name, "☣️", debug_color);
             break;
     }
+
+    if (log_level == LogLevel.CRITICAL || log_level == LogLevel.ERROR) {
+        process.stderr.write(header + " " + data + "\n");
+        delta = 0;
+    } else {
+
+        const output = header + " " + data + "\n";
+
+        if (REWRITE) {
+            const diff = -delta;
+            delta = [...output.matchAll(/\n/g)].length;
+            process.stdout.moveCursor(0, diff);
+            process.stdout.clearScreenDown();
+            process.stdout.write(output);
+        } else {
+            process.stdout.write(output);
+            delta = 0;
+        }
+    }
+
 };
 function construct_log_header(type, logger_name: string, emoji, color: string): any {
 
