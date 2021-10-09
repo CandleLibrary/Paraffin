0e5a18580e2fb8539c8b7cb6e15aef6794ea80f0
Anthony Weathersby <acweathersby.codes@gmail.com>
2021-10-08 21:44:31 -0600

    Refactor: Rename import reference

--- source/typescript/utils/get_package_json.ts
+++ source/typescript/utils/get_package_json.ts
@@ -1,9 +1,8 @@
-import URL from "@candlelib/uri";
-
-import { PackageJSONData } from "../types/package";
+import URI from "@candlelib/uri";
 //@ts-ignore
 import fs from "fs";
-import URI from "@candlelib/uri";
+import { PackageJSONData } from "../types/package";
+
 
 const fsp = fs.promises;
 
@@ -11,16 +10,16 @@ const fsp = fs.promises;
  * Locates the nearest package.json file. Searches up the directory structure until one is found.
  * If no package.json file can be found, then the return object property FOUND will be false.
  */
-export async function getPackageJsonObject(cwd: string | URI = URL.getCWDURL())
+export async function getPackageJsonObject(cwd: string | URI = URI.getCWDURL())
     : Promise<{ package: PackageJSONData, package_dir: string; FOUND: boolean; }> {
 
-    await URL.server();
+    await URI.server();
 
     if (typeof cwd == "string")
-        cwd = new URL(cwd);
+        cwd = new URI(cwd);
 
     let
-        pkg_file_path: URL = URL.resolveRelative("./package.json", cwd.dir),
+        pkg_file_path: URI = URI.resolveRelative("./package.json", cwd.dir),
         i = pkg_file_path.path.split("/").filter(s => s !== "..").length,
         pkg: PackageJSONData = null,
         FOUND = false;
@@ -38,13 +37,13 @@ export async function getPackageJsonObject(cwd: string | URI = URL.getCWDURL())
         } catch (e) {
             //Suppress errors - Don't really care if there is no file found. That can be handled by the consumer.
         }
-        pkg_file_path = URL.resolveRelative("../package.json", pkg_file_path);
+        pkg_file_path = URI.resolveRelative("../package.json", pkg_file_path);
     }
 
     return { package: <PackageJSONData>pkg, package_dir: FOUND ? pkg_file_path.dir : "", FOUND };
 }
 
-export async function savePackageJSON(pkg: PackageJSONData, directory: string = URL.getCWDURL() + "") {
+export async function savePackageJSON(pkg: PackageJSONData, directory: string = URI.getCWDURL() + "") {
     const { package_dir } = await getPackageJsonObject(directory);
 
     await fsp.writeFile(package_dir + "package.json", JSON.stringify(pkg));
